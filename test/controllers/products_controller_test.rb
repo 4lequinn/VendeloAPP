13ea0a23cccfea4426c4526738e3a6de32db2f42
Jorge Quintui <jorgealequinn@gmail.com>
2022-09-26 16:33:31 -0300

    ADD: Se crean Pruebas Unitarias

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+    test 'render a list of products' do
+        get products_path
+
+        assert_response :success  
+        assert_select '.product', 2
+    end 
+
+    test 'render a detail product page' do
+        get product_path(products(:ps4)) # products(:ps4).id funciona así también
+        assert_response :success
+        assert_select '.title', 'PS4 FAT'
+        assert_select '.description', 'PS4 En buen estado'
+        assert_select '.price', '1000' 
+    end
+
+end
