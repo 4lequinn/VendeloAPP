867af6f5ba625e278e12e5f54cd025520b7032e9
Jorge Quintui <jorgealequinn@gmail.com>
2022-09-26 15:41:21 -0300

    UPDATE: Se listan todos los productos de la DB

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
     def index
-        
+        # Variable de instancia, se puede leer desde nuestra vista
+        @products = Product.all
     end
 end
